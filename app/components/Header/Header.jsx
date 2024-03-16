3cb893b10cf3ed3b20cdeedf64f1188c79d84ea0
s3reg <149339169+s3reg@users.noreply.github.com>
2024-03-16 17:03:01 +0400

    v3
    v3

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { AuthForm } from '../AuthForm/AuthForm';
+import { Overlay } from '../Overlay/Overlay';
+import { Popup } from '../Popup/Popup';
 import Styles from './Header.module.css'
+import { useState } from 'react';
 
 export const Header = () => {
+  const [popupIsOpened, setpopupIsOpened] = useState(false);
+  const openPopup = () => {
+    setpopupIsOpened(true);
+  };
+  const closePopup = () => {
+    setpopupIsOpened(false);
+  };
+
   return (
     <header className={Styles['header']}>
       <a href="./index.html" className={Styles['logo']}>
@@ -44,9 +58,15 @@ export const Header = () => {
           </li>
         </ul>
         <div className={Styles['auth']}>
-          <button className={Styles['auth__button']}>Войти</button>
+          <button className={Styles['auth__button']} onClick={openPopup}>
+            Войти
+          </button>
         </div>
       </nav>
+      <Overlay popupIsOpened={popupIsOpened} closePopup={closePopup}/>
+      <Popup popupIsOpened={popupIsOpened} closePopup={closePopup}>
+        <AuthForm />
+      </Popup>
     </header>
   )
 }
